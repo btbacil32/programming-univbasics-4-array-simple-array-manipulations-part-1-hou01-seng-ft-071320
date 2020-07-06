fd28feeb74b040f3168c3fa317701746fb9596f9
bacil thalji <btbacil32@gmail.com>
2020-07-06 00:19:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+require 'pry'
+
+def using_push(colors_in_the_rainbow,next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(bouroughs_in_nyc,new_neighborhood)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(continents_array)
+    binding.pry
+  continents_array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents_array.pop
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = dog_breeds.pop("Chihuahua","Shiba Inu")
+end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  im_so_over_this_city = my_favorite_cities.shift
+  im_so_over_this_city
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  brands_removed = ice_cream_brands.shift
+end
+
+
+
+
+
+
+
+
+
+
